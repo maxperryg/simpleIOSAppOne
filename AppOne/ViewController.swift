b5dfa4863a7faee1c1ef0a95c4ec14c1db1a73a2
Maxwell Perry Grossman <MaxGrossman@maxwells-air.lan>
2018-06-13 12:24:55 -0400

    Initial Commit

---
+++ AppOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Maxwell Perry Grossman on 6/13/18.
+//  Copyright © 2018 Maxwell Perry Grossman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
